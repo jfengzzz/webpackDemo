6d39b4e8a4f64eb2ecde4d50436ad7e9843e409e
jfengzzz <1067437442@qq.com>
2020-07-01 12:13:15 +0800

    更新 SplitChunksPlugin 代码分割

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -37,15 +37,38 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html',
       hash: true,
-      chunks: ['index']
+      chunks: ['index', 'commons', 'vendors']
     }),
     // page1.html
     new HtmlWebpackPlugin({
       template: './src/pageOne.html',
       filename: 'pageOne.html',
       hash: true,
-      chunks: ['pageOne']
+      chunks: ['pageOne', 'commons', 'vendors']
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'initial',
+      minSize: 3, // 模块的最小体积
+      minChunks: 2, // 模块的最小被引用次数
+      maxAsyncRequests: 5, // 按需加载的最大并行请求数
+      maxInitialRequests: 3, // 一个入口最大并行请求数
+      automaticNameDelimiter: '~', // 文件名的连接符
+      cacheGroups: { // 缓存组
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          name: 'vendors'
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+          name: 'commons'
+        }
+      }
+    }
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,8 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
         // use: ExtractTextPlugin.extract({
-        //   fallback: "style-loader",
-        //   use: "css-loader"
+        //   fallback: 'style-loader',
+        //   use: 'css-loader'
         // })
       },
       {
